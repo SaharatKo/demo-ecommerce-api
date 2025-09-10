c6fa90fbf6c22143046cef54c3b41f935a2d553c
saharat.k <saharat.kosiya@gmail.com>
2025-09-10 21:22:06 +0700

    Fix jenkinsfile - zip not found

--- cicd/terraform/root/backend.tf
+++ cicd/terraform/root/backend.tf
@@ -1,9 +1,9 @@
 terraform {
     backend "s3" {
         bucket = "demo-ecommerce-api-s3"
-        key = "myapi/terraform.tfstate"
+        key = "demo-ecommerce-api-s3/terraform.tfstate"
         region = "ap-southeast-1"
-        dynamodb_table = "demo-ecommerce-api-tf-locks" # for state locking
+        dynamodb_table = "demo-ecommerce-api-tf-locks"
         encrypt = true
     }
 }
